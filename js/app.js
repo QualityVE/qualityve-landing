ea5a65737722a4607cc58de458b5a36a63cda11a
Diego Castro <castrogarcia2017gmail.com>
2021-06-19 21:09:24 -0400

    haciendo quality responsive.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 //variables
+const nav = document.querySelector('.header__nav');
 let body = document.querySelector('body');
+const burger = document.querySelector('.burger');
+const headerInfo = document.querySelector('.header__info');
+
+const info = ['About Us', 'Services', 'References'];
 
 //listeners
 cargarEventListener();
@@ -11,6 +16,45 @@ function cargarEventListener()
 
 //functions
 
-function medirAnchoBody(e) {
-    console.log(body.clientWidth);
+function medirAnchoBody() {
+
+    let anchoBody = body.clientWidth;
+    console.log(anchoBody);
+
+    if(anchoBody < 600)
+    {
+        if(headerInfo)
+        {
+            while(headerInfo.firstChild)
+            {
+                headerInfo.remove(headerInfo.firstChild);
+            }
+        }
+
+        if(!burger.firstChild)
+        {
+            const span = document.createElement('span');
+            span.classList.add('burger-icon');
+            burger.appendChild(div);
+        }
+
+    }
+
+    if(anchoBody >= 600 && anchoBody < 800)
+    {
+
+        if(!headerInfo.firstChild)
+        {
+            info.forEach(function(item){
+                const li = document.createElement('li');
+                li.innerHTML = 
+                `
+                    <a href="#">${item} <span></span> </a>
+                `;
+
+                
+            });
+        }
+    }
+
 }
